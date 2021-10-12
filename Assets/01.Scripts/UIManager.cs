414ff8e65534533f7a302abf5a910094b5c3b5b2
Sang <64353008+sky10735@users.noreply.github.com>
2021-10-12 16:14:54 +0900

    UI 반완성
    UI 반완성 후 업로드
    (점수연동, 재시작, 플레이어 소환 미완료)

--- Assets/01.Scripts/UIManager.cs
+++ Assets/01.Scripts/UIManager.cs
@@ -1,16 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public CanvasGroup title;
+    public CanvasGroup inGame;
+    public CanvasGroup gameOver;
+
+    public Text titleText;
+    public Text start;
+    public Camera cam;
+
+    [TextArea]
+    public string text;
+
     void Start()
     {
-        
+        cam = Camera.main;
+        titleText.DOText(text, 3f);
     }
 
     void Update()
     {
-        
+
+    }
+    public void TitlePanel()
+    {
+        title.interactable = false;
+        title.blocksRaycasts = false;
+        StartCoroutine(CheckTime(title));
+    }
+
+    public void GameOver()
+    {
+        inGame.interactable = false;
+        inGame.blocksRaycasts = false;
+        StartCoroutine(CheckTime(inGame));
+    }
+
+    public void Restart()
+    {
+        gameOver.interactable = false;
+        gameOver.blocksRaycasts = false;
+        StartCoroutine(CheckTime(gameOver));
+    }
+
+    IEnumerator CheckTime(CanvasGroup cGroup)
+    {
+        while(cGroup.alpha > 0)
+        {
+            cGroup.alpha -= 0.07f;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
