2dfc20f08ada88b1e189aeb313b43012cc83c725
Anglik <seonwoo6429@gmail.com>
2021-10-21 10:50:04 +0900

    미터기 + 카메라 수정중

--- Assets/01.Scripts/Player.cs
+++ Assets/01.Scripts/Player.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public bool isCling;
-    public float upSpeed = 1f;
-    public float moveSpeed = 1f;
+    public bool isCling = false; //먹이에 붙어있는지
+    public float upSpeed = 1f; //상하로 이동하는 속도 
+    public float moveSpeed = 1f; //좌우로 이동하는 속도
+
+    private MoveDir moveDir;
+
+    private void Start()
+    {
+        InputManager.instance.onClick += () =>
+        {
+            moveDir = InputManager.GetDir();
+
+            if(isCling)
+            {
+                //isCling = false;
+            }
+        };
+    }
 
     private void Update()
     {
+        if(moveDir == MoveDir.Left)
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+
         if(!isCling)
         {
             transform.Translate(Vector2.up * upSpeed * Time.deltaTime);
