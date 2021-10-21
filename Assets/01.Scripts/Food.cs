3ac8c7ff20754a303b7b24d5a8fe15eec15e43a0
Anglik <seonwoo6429@gmail.com>
2021-10-21 11:51:33 +0900

    먹이에 닿아서 움직이는거 구현

--- Assets/01.Scripts/Food.cs
+++ Assets/01.Scripts/Food.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public bool isCling;
-    public float downSpeed;
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private Player player;
+
+    public bool isCling = false;
+    public float orginSpeed = 1.5f;
+    public float clingSpeed;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        player = GameManager.instance.player;
+        
+        clingSpeed = orginSpeed * 0.75f;
+    }
 
     private void Update()
     {
-        if(!isCling)
+        if(isCling)
         {
-            transform.Translate(Vector2.up * downSpeed * Time.deltaTime);
+            transform.Translate(Vector2.down * clingSpeed * Time.deltaTime);
         }
         else
         {
-            transform.Translate(Vector2.up * (downSpeed * 0.75f) * Time.deltaTime);
+            transform.Translate(Vector2.down * orginSpeed * Time.deltaTime);
         }
+    }
+
+    public void Init(Vector3 pos)
+    {
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        transform.position = pos;
     }
 }
